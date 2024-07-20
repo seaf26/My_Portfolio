c3e2ebc849b934ed51e52b5a7e4ad678d5071472
Seaf mohamed Gamel Abdullah <seaf222101230@ksiu.edu.eg>
2024-07-20 16:24:50 +0300

    fix: Update CV download link in main.js

--- Assets/main.js
+++ Assets/main.js
@@ -26,7 +26,7 @@ var typed = new Typed('.typedText', {
 // ? Dowmload CV
 document.getElementById("downloadButton").addEventListener("click", function() {
     var link = document.createElement('a');
-  link.setAttribute('href', 'https://drive.google.com/file/d/1ul_ZZmWzsCaYF4r7OHahL9Sc8J5z4u2s/view?usp=sharing');
+  link.setAttribute('href', 'https://drive.google.com/file/d/1SUMOFMz1TX5ZSuzbbU5GRJcBOWnCmlLZ/view?usp=sharing');
   link.setAttribute('download', 'Seaf_GamelCV.pdf');
   link.setAttribute('rel', 'noopener');
   link.click();
